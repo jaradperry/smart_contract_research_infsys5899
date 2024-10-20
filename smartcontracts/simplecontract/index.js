ebd675a3923e4929a8926110f02bc2e0d05a3df3
jaradperry <jaradmperry@outlook.com>
2024-10-20 18:53:22 -0500

    Removed webapp.js and modified index.js

--- smartcontracts/simplecontract/index.js
+++ smartcontracts/simplecontract/index.js
@@ -1,5 +1,5 @@
 'use strict';
 
-const VehicleContract = require('./vehicleContract2'); // Adjust the path if necessary
+const VehicleContract = require('./vehicleContract2');
 
 module.exports.contracts = [ VehicleContract ];
